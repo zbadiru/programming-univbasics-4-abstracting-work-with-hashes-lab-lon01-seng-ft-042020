619b85505b9beec489ab355474bb7855ea26ba25
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 11:13:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def my_hash_creator(key, value)
   {key => value}
-  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
+  hash = {:key => value, :key_two => value_two} 
   # return the correct value using the hash and key parameters
 end
 
